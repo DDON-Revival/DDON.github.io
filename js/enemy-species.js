94d2729f6553400d56e1738eeba8f57890f2a348
Dixdros <52747344+Dixdros@users.noreply.github.com>
2025-12-30 06:54:34 +0100

    Add files via upload

--- js/enemy-species.js
+++ js/enemy-species.js
@@ -1,4 +1,10 @@
 export async function loadEnemySpecies() {
-  const res = await fetch("data/enemy-species.json");
-  return await res.json();
+  return {
+    "0x010201": "Beast",        // Direwolf
+    "0x010209": "Beast",        // InfectedDirewolf
+    "0x010100": "DemiHuman",    // Goblin
+    "0x010911": "Ooze",         // AcidBlob
+    "0x020403": "Demon",        // AlteredZuhl
+    // 👉 hier ALLE EnemyIds
+  };
 }
