c8e24f1636c4c519c3abd86496fc15c6914f0b7c
Dixdros <52747344+Dixdros@users.noreply.github.com>
2026-02-16 23:15:56 +0100

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -6,19 +6,13 @@ async function loadJSON(name) {
 }
 
 async function loadAll() {
+
     await loadJSON("EnemySpawn.json");
     await loadJSON("enemy-names.json");
     await loadJSON("stage-names.json");
-    await loadJSON("item_name.toml");
 
     console.log("All data loaded.");
     window.dataLoaded = true;
-}
-
-    // 🔥 WICHTIG:
-    if (window.initWiki) {
-        window.initWiki();
-    }
 }
 
 loadAll();
