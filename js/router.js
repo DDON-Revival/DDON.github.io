c426e26f855b5562b9e07fe05baf3e237e1414ff
Dixdros <52747344+Dixdros@users.noreply.github.com>
2026-02-19 16:12:08 +0100

    Add files via upload

---
+++ js/router.js
@@ -0,0 +1,44 @@
+function navigate(url){
+    history.pushState({}, "", url);
+    router();
+}
+
+window.addEventListener("popstate", router);
+
+function router(){
+
+    if (!window.dataLoaded){
+        setTimeout(router,50);
+        return;
+    }
+
+    const params = new URLSearchParams(window.location.search);
+
+    if (params.has("monster")){
+        openMonster(params.get("monster"));
+        return;
+    }
+
+    if (params.has("item")){
+        openItem(params.get("item"));
+        return;
+    }
+
+    if (params.has("stage")){
+        openStage(params.get("stage"));
+        return;
+    }
+
+    if (params.has("shop")){
+        openShop(params.get("shop"));
+        return;
+    }
+
+    if (params.has("special")){
+        openSpecialShop(params.get("special"));
+        return;
+    }
+
+    renderMonsterList();
+}
+
