2890334abeff4c9d3b0b989ee6c8fd69d8684511
Dixdros <52747344+Dixdros@users.noreply.github.com>
2026-02-19 16:13:05 +0100

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -14,31 +14,56 @@ function router(){
 
     const params = new URLSearchParams(window.location.search);
 
+    /* ================= MONSTER ================= */
+
     if (params.has("monster")){
         openMonster(params.get("monster"));
         return;
     }
+
+    /* ================= ITEM ================= */
 
     if (params.has("item")){
         openItem(params.get("item"));
         return;
     }
 
+    /* ================= STAGE ================= */
+
     if (params.has("stage")){
         openStage(params.get("stage"));
         return;
     }
+
+    /* ================= SHOP LIST ================= */
+
+    if (params.has("shops")){
+        renderShopList();
+        return;
+    }
+
+    /* ================= SINGLE SHOP ================= */
 
     if (params.has("shop")){
         openShop(params.get("shop"));
         return;
     }
 
+    /* ================= SPECIAL LIST ================= */
+
+    if (params.has("special") && params.get("special")===""){
+        renderSpecialShopList();
+        return;
+    }
+
+    /* ================= SINGLE SPECIAL ================= */
+
     if (params.has("special")){
         openSpecialShop(params.get("special"));
         return;
     }
 
+    /* ================= DEFAULT ================= */
+
     renderMonsterList();
 }
-
